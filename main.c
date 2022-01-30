34d0f891b1f5dcee74d013b67b8e4d195e4991c4
Michal Němec <michaelnemec47@gmail.com>
2022-01-30 14:11:14 +0100

    Wait for use intup with curses

--- main.c
+++ main.c
@@ -11,68 +11,58 @@ const int PAUSE_LONG_MIN = 15;
 const int POMODORO_MIN = 25;
 const int REPETITION_COUNT = 4;
 
-void run(char *mode, int minutes) {
-  printf("Starting %s\n", mode);
+void run(WINDOW *w, char *mode, int minutes) {
+  nodelay(w, TRUE);
+
+  printw("Starting %s\n", mode);
   for (int sec = 0; sec < minutes / 5; sec++) {
     sleep(1);
-    printf("sec: %d\n", sec);
+    clear();
+    printw("%s %d:%02d\n", mode, sec / 60, sec);
+
+    char c;
+    while ((c = getch()) != ERR) {
+      if (c == 'q')
+        endwin();
+      exit(0);
+    }
   }
   printf("\a%s ended\n", mode);
-  fflush(stdin);
-}
 
-void setup_nonbuffering_IO() {
-  struct termios old_tio, new_tio;
-
-  /* get the terminal settings for stdin */
-  tcgetattr(STDIN_FILENO, &old_tio);
-
-  /* we want to keep the old setting to restore them a the end */
-  new_tio = old_tio;
-
-  /* disable canonical mode (buffered i/o) and local echo */
-  new_tio.c_lflag &= (~ICANON & ~ECHO);
-
-  /* set the new settings immediately */
-  tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
-
-  /* restore the former settings */
-  // tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
-}
-
-void waitJirka() {
-  scanf(" ");
-  fflush(stdin);
+  nodelay(w, FALSE);
 }
 
 int main(void) {
-  bool program_run = true;
-  setup_nonbuffering_IO();
+  WINDOW *w = initscr();
+  nodelay(w, FALSE);
 
-  printf("Starting pomodoro, waiting on enter...\n");
-  waitJirka();
+  bool program_run = true;
+
+  printw("Starting pomodoro, waiting on enter...\n");
+  getch();
 
   while (program_run) {
-    printf("Looping \n");
+    printw("Looping \n");
 
     for (int i = 0; i < REPETITION_COUNT; i++) {
-      run("Pomodoro:", POMODORO_MIN);
-      printf("waiting on enter");
-      waitJirka();
+      run(w, "Pomodoro:", POMODORO_MIN);
+      printw("waiting on enter\n");
+      getch();
 
       if (i == REPETITION_COUNT - 1) {
         program_run = false;
         break;
       }
 
-      run("Pause:", PAUSE_MIN);
-      printf("waiting on enter,to start again\n");
+      run(w, "Pause:", PAUSE_MIN);
+      printw("waiting on enter,to start again\n");
     }
 
-    run("PauseLong:", PAUSE_LONG_MIN);
-    printf("nice end\n");
-    waitJirka();
+    run(w, "PauseLong:", PAUSE_LONG_MIN);
+    printw("nice end\n");
+    getch();
   }
 
+  endwin();
   return 0;
 }
