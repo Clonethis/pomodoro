f2842a4cd8cf37da15e776bbbeefd10269efa1d0
Clonethis <jirka.brtnik@post.cz>
2022-01-30 13:27:07 +0100

    waiting routine

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <unistd.h>
 #include <curses.h>
 #include <string.h>
+#include <termios.h>
+#include <stdbool.h>
 
 const int PAUSE_MIN = 5;
 const int PAUSE_LONG_MIN = 15;
@@ -10,35 +12,67 @@ const int POMODORO_MIN = 25;
 const int REPETITION_COUNT = 4;
 
 void run(char *mode,int minutes){
-    printf("%s\n",mode);
-    sleep(1);
-    printf("Mode ended\n");
+    printf("Starting %s\n", mode);
+    for (int sec = 0; sec < minutes / 5; sec++) {
+      sleep(1);
+      printf("sec: %d\n", sec);
+    }
+    printf("\a%s ended\n", mode);
+    fflush(stdin);
 }
 
+void setup_nonbuffering_IO() {
+	struct termios old_tio, new_tio;
+
+	/* get the terminal settings for stdin */
+	tcgetattr(STDIN_FILENO,&old_tio);
+
+	/* we want to keep the old setting to restore them a the end */
+	new_tio=old_tio;
+
+	/* disable canonical mode (buffered i/o) and local echo */
+	new_tio.c_lflag &=(~ICANON & ~ECHO);
+
+	/* set the new settings immediately */
+	tcsetattr(STDIN_FILENO,TCSANOW,&new_tio);
+
+	/* restore the former settings */
+	// tcsetattr(STDIN_FILENO,TCSANOW,&old_tio);
+}
+void waitJirka(){
+    scanf(" ");
+  fflush(stdin);
+}
 int main(void) {
+  bool program_run = true;
+  setup_nonbuffering_IO();
 
   printf("Starting pomodoro, waiting on enter...\n");
+   waitJirka();
 
-
-  while(1){
+  while(program_run){
       printf("Looping \n");
-        // wait();
+
+
       for(int i =0; i<REPETITION_COUNT; i++){
           run("Pomodoro:",POMODORO_MIN);
           printf("waiting on enter");
-;
-            if(i == REPETITION_COUNT-1){
+           waitJirka();
 
-              exit(1);
+          if(i == REPETITION_COUNT-1){
+              program_run=false;
+              break;
 
-            }
+          }
+
           run("Pause:",PAUSE_MIN);
           printf("waiting on enter,to start again\n");
 
       }
 
-        run("PauseLong:",PAUSE_LONG_MIN);
-        printf("nice end\n");
+      run("PauseLong:",PAUSE_LONG_MIN);
+      printf("nice end\n");
+       waitJirka();
 
   }
 
