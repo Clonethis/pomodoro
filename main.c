80490d5650a672f26afb9f4dde41ac94af9d7e69
Michal Němec <michaelnemec47@gmail.com>
2022-01-30 15:38:53 +0100

    Lower cpu conspumtion and add s for skip

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <curses.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -22,7 +23,7 @@ int duration(MODE m) {
   case PAUSE_LONG:
     return 5;
   case POMODORO:
-    return 5;
+    return 25;
   }
 }
 
@@ -31,10 +32,14 @@ char *name(MODE m) { return names[m]; }
 void play_bell(MODE m) {
   switch (m) {
   case PAUSE:
-    printf("\a \a");
+    printf("\a");
+    usleep(500000);
+    printf("\a");
     break;
   case PAUSE_LONG:
-    printf("\a \a");
+    printf("\a");
+    usleep(500000);
+    printf("\a");
     break;
   case POMODORO:
     printf("\a");
@@ -45,18 +50,29 @@ void play_bell(MODE m) {
 void run(WINDOW *w, MODE m) {
   nodelay(w, TRUE);
 
-  for (int msec = duration(m) * 1000; msec >= 0; msec--) {
-    int sec = (msec / 1000) + 1;
-    clear();
-    printw("%s %d:%02d\n", name(m), sec / 60, sec);
+  for (int msec = duration(m) * 60 * 1000; msec >= 0; msec--) {
+    if (msec % 1000 == 0) {
+      clear();
+      int sec = msec / 1000;
+      printw("%s %d:%02d\n", name(m), sec / 60, sec % 60);
+    }
+
     usleep(1000);
 
     char c;
+    bool skip = false;
     while ((c = getch()) != ERR) {
       if (c == 'q') {
         endwin();
         exit(0);
       }
+      if (c == 's') {
+        skip = true;
+      }
+    }
+
+    if (skip) {
+      break;
     }
   }
 
@@ -72,10 +88,17 @@ int main(void) {
   WINDOW *w = initscr();
   nodelay(w, FALSE);
 
-  bool program_run = true;
-
-  printw("Ready for pomodoro, press enter to start...\n");
+  printw("Pomodoro\n"
+         "\n"
+         "# Controls\n"
+         "Press:\n"
+         " - 'q' for quit\n"
+         " - 's' for skip\n"
+         "\n"
+         "Ready for pomodoro, press enter to start...");
   getch();
+
+  bool program_run = true;
 
   while (program_run) {
     printw("Looping \n");
